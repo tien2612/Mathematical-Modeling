8dc9ab02ff4d8b01921491afa32653796a74e104
Nguyen Phuc Tien <nguyenphuctien1258@gmail.com>
2022-11-17 23:17:09 +0700

    example 1

--- source code/main.py
+++ source code/main.py
@@ -5,30 +5,35 @@ import matplotlib.colors
 import math
 
 # function that returns dz/dt
-t = np.arange(0,100*np.pi, 1)
+t = np.arange(0, 10)
 def model(z, t):
-    R = pow(np.e, 3*t) * ( -np.cos(t) - np.sin(t) )
-    J = pow(np.e, 3*t) * ( 3 * np.cos(t) +  2*np.sin(t) )
-    Rdot = 2 * R + 2 * J
-    Jdot = -1 * R + 4*J
+    #example 1
+    a = 1
+    b = 6
+    c = 1
+    d = -4
+    c1 = 1
+    c2 = -1
+    R = 6 * c1 * pow(np.e, 2*t) + 3 * c2 * pow(math.e, -3 * t)
+    J = c1 * pow(np.e, 2*t) + 2 * c2 * pow(math.e, -3 * t)
+    Rdot = a * R + b * J
+    Jdot = c * R + d * J
     dzdt = [Rdot, Jdot]
     return dzdt
 
-
 # initial condition
-z0 = [-1, 3]
+z0 = [3, -1]
 
 # time points
-t = np.linspace(0, 6, 1000)
+t = np.linspace(0, 10, 100)
 # solve ODE
 z = odeint(model, z0, t)
 
 # plot results
 plt.plot(t, z[:,0],'c-',label=r"Romeo's")
 plt.plot(t, z[:,1], color = 'orange', label=r"Juliet's")
-plt.title("LOVE BETWEEN A EAGER BEAGER AND A NARCISSISTIC NERD", fontsize = 12)
+plt.title("LOVE BETWEEN A EAGER BEAGER AND A CAUTIONS LOVER", fontsize = 12)
 plt.ylabel('Love for the other')
-plt.xlabel('Time')
 plt.legend(loc='best')
 plt.grid()
 plt.show()
